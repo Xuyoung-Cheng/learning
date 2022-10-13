851d24cf9c494a2af0442e756f659c55558ec6b3
simoncheng <simoncheng1024@163.com>
2022-10-13 17:51:44 +0800

    NCCL: lesson 1.
    - The simplest example of a C program.
    - Variable definition and use: local varialbes, global variables, how to use them
      and their differences.
    - Basic concepts and commands related to program compliation and
      execution.
    	- gcc c_source_file.
    	- objcopy -O binary -j [.text|.data] object filename :
    	  copy and translate object files.
    	- hexdump -C object : display file contents in hexadecimal,
    	  decimal, octal, or ascii.
    	- file : determine file type.
    	- readelf -a object : display information about ELF files.
    - Shell built-in variable $?, get the program exit status.

---
+++ c/NCCL/variable.c
@@ -0,0 +1,9 @@
+/* global variable definition */
+int global = 2;
+
+int main(void)
+{
+	/* local variable definition */
+	int local = 1;
+	return local + global;
+}
---
+++ c/NCCL/variable2.c
@@ -0,0 +1,8 @@
+
+int main(void)
+{
+	/* local variable definition */
+	int local = 1;
+	int global = 2;
+	return local + global;
+}
