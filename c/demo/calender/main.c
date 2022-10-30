57d59e84d147c280fec269fe0825b298a1de3c21
simoncheng <simoncheng1024@163.com>
2022-10-30 22:08:19 +0800

    C demo: calender.
    	- second step: Calculate how many days have passed since the beginning of the year.

--- c/demo/calender/main.c
+++ c/demo/calender/main.c
@@ -7,6 +7,33 @@ int isleap(int y)
 	return ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) ? 1 : 0;
 }
 
+int max_day(int y, int m)
+{
+	int ret;
+	int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	ret = isleap(y);
+	if (ret == LE_YEAR)
+		month[1] = 29;
+
+	return month[m - 1];
+}
+
+int total_day(int y, int m, int d)
+{
+	int sum;
+	int i;
+
+	sum = 0;
+	for (i = 1; i < m; i++)
+	{
+		sum += max_day(y, i);
+	}
+	sum += d;
+
+	return sum;
+}
+
 int main(int argc, char *argv[])
 {
 	int year = 0, month = 0, day = 0;
@@ -26,5 +53,8 @@ int main(int argc, char *argv[])
 		printf("%d-%d-%d is leap year\n", year, month, day);
 	else
 		printf("%d-%d-%d is not leap year\n", year, month, day);
+	
+	ret = total_day(year, month, day);
+	printf("%d-%d-%d is %d day of the year\n", year, month, day, ret);
 	return 0;
 }
